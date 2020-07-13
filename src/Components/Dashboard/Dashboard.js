c164278b762d661d4eec92adf81955b5094252c5
awpala <adam.w.pala@gmail.com>
2020-07-13 16:45:41 -0500

    Completed Part 2 Step 1

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,71 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            searchTerm: '',
+            isMyPost: true,
+            posts: []
+        };
+    }
+
+    // Event Handlers
+    handleInput = e => {
+        this.setState({ searchTerm: e.target.value });
+    }
+
+    handleSearch = () => {
+        // TO-DO
+    }
+
+    handleReset = () => {
+        this.setState({ searchTerm: '' })
+    }
+
+    handleCheck = () => {
+        this.setState({ isMyPost: !this.state.isMyPost });
+    }
+
     render() {
-        return <div>Dashboard</div>
+        const mappedPosts = this.state.posts.map(post => (
+            <div className='posts'>
+                <p>{post.title}</p>
+                <p>by {post.author_id}</p>
+                <img key={post.post_id} src={post.profile_picture} alt='post'/>
+            </div>
+        ));
+
+        return (
+        <div>
+            <input
+                type='text'
+                value={this.state.searchTerm}
+                name='search-term'
+                placeholder='Search by Title'
+                onChange={e => this.handleInput(e)}
+            />
+            <div className='search-buttons'>
+                <button onClick={this.handleSearch}>Search</button>
+                <button onClick={this.handleReset}>Reset</button>
+                <p>My Posts</p>
+                <input
+                    type='checkbox'
+                    value={this.state.isMyPost}
+                    name='my-posts'
+                    onChange={this.handleCheck}
+                />
+            </div>
+            <div className='posts'>
+                {mappedPosts}
+            </div>
+        </div>
+        );
     }
 };
 
-export default Dashboard;
+const mapStateToProps = reduxState => reduxState;
+
+export default connect(mapStateToProps)(Dashboard);
